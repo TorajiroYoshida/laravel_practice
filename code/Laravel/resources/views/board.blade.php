9c2c32f636b019ad03af621861a14619f48b4c39
Tomoyuki Sueda <fkenf339@yahoo.co.jp>
2018-07-12 16:44:23 +0900

    view追加

--- code/Laravel/resources/views/board.blade.php
+++ code/Laravel/resources/views/board.blade.php
@@ -5,10 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Board</title>
+        <title>@yield('title')</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/css/styles.css">
 
         <!-- Styles -->
         <style>
@@ -16,6 +17,12 @@
 
     </head>
     <body>
-        test
+        <div id="header">
+            @yield('header')
+            <h1>掲示板</h1>
+        </div><!-- header -->
+        <div id="main">
+            @yield('content')
+        </div><!-- main -->
     </body>
 </html>
